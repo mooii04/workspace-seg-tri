bf3ad6d92488a7abef4bafafa7d59eb18208967a
MoisésDorado <dorado.gumoi24@triana.salesianos.edu>
2024-02-01 17:15:52 +0100

    subida en casa

---
+++ Unidad5/EjemplosUD5/src/ejemploList/Alumno.java
@@ -0,0 +1,16 @@
+package ejemploList;
+
+public class Alumno {
+
+	private String nombre;
+	private String apellidos;
+	private int codAlum;
+	
+	public Alumno(String nombre, String apellidos, int codAlum) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.codAlum = codAlum;
+	}
+	
+}
