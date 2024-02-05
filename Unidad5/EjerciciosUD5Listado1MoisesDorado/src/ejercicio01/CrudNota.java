be2c281b4c8ed68e5f7ff307f1deb5da9316e80e
MoisesDorado <dorado.gumoi24@triana.salesianos.edu>
2024-02-05 14:23:26 +0100

    asdfsdf

--- Unidad5/EjerciciosUD5Listado1MoisesDorado/src/ejercicio01/CrudNota.java
+++ Unidad5/EjerciciosUD5Listado1MoisesDorado/src/ejercicio01/CrudNota.java
@@ -1,44 +1,46 @@
 package ejercicio01;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class CrudNota {
-
-	List <Nota> lista = new ArrayList <Nota> ();
-
+	
+	private List<Nota> lista;
+	
 	public CrudNota(List<Nota> lista) {
-		super();
 		this.lista = lista;
 	}
-
+	
 	public List<Nota> getLista() {
 		return lista;
 	}
-
+	
 	public void setLista(List<Nota> lista) {
 		this.lista = lista;
 	}
-
+	
 	@Override
 	public String toString() {
 		return "CrudNota [lista=" + lista + "]";
 	}
 	
-	public void imprimirLista() {
-		for (Nota n : lista) {
-			System.out.println(n);
-		}
+	public void imprimirNotas() {
+		System.out.println(lista);
+		System.out.println();
 	}
 	
-	public void addPersonaV1 (Nota n) {
-		int cont=0;
+	public void agregar(Nota n) {
 		lista.add(n);
-		cont ++;
+		aumentarContador();
 	}
 	
-	public void mostrarContador () {
-		
+	public int aumentarContador() {
+		int contador=0;
+		contador++;
+		return contador;
+	}
+	
+	public void mostrarContador() {
+		System.out.println(aumentarContador());
 	}
 	
 	public void borrar (int id) {
--- Unidad5/EjerciciosUD5Listado1MoisesDorado/src/ejercicio01/Principal.java
+++ Unidad5/EjerciciosUD5Listado1MoisesDorado/src/ejercicio01/Principal.java
@@ -3,12 +3,45 @@ package ejercicio01;
 import java.util.ArrayList;
 import java.util.List;
 
+import utilidades.Leer;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		List <Nota> lista = new ArrayList <Nota> ();
+		int id=0;
+		
+		List <Nota> lista = new ArrayList<>();
+		
+		Nota n1 = new Nota("Hola", 1, "Bruno");
+		Nota n2 = new Nota("Prueba", 2, "Messi");
+		Nota n3 = new Nota("Adiós", 3, "Bruno");
+		
+		CrudNota cn = new CrudNota (lista);
+		
+		cn.agregar(n1);
+		cn.agregar(n2);
+		cn.agregar(n3);
+		
+		cn.imprimirNotas();
+		
+		System.out.println();
+		
+		System.out.println(cn.aumentarContador());
+		
+		
+		System.out.println("Cual borras");
+		
+		id=Leer.datoInt();
+		
+		cn.borrar(id);
+		
+		cn.imprimirNotas();
+		
+		System.out.println();
+		
+		System.out.println(cn.aumentarContador());
 		
 		
 	}
