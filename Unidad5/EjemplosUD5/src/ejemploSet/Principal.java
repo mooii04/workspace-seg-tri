3ea2148d159bb5cc5c618b155d36832d623c332d
MoisesDorado <dorado.gumoi24@triana.salesianos.edu>
2024-02-09 08:58:12 +0100

    asdasde

--- Unidad5/EjemplosUD5/src/ejemploSet/Principal.java
+++ Unidad5/EjemplosUD5/src/ejemploSet/Principal.java
@@ -10,7 +10,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Set <String> lista = new HashSet <String>();
+		//Set <String> lista = new HashSet <String>();
 		
 		Set <Persona> listaP = new HashSet <Persona>();
 		int dni;
