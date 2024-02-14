35a6333b7217ad456e68ff9c02273842d2964782
MoisesDorado <dorado.gumoi24@triana.salesianos.edu>
2024-02-14 10:59:44 +0100

    asdasd

---
+++ Unidad5/EjemplosUD5/src/ejemploOrdenar/ComparaPorMarca.java
@@ -0,0 +1,27 @@
+package ejemploOrdenar;
+
+import java.util.Comparator;
+
+public class ComparaPorMarca implements Comparator<Corredor> {
+
+	@Override
+	public int compare(Corredor c1, Corredor c2) {
+		// TODO Auto-generated method stub
+		//Creamos las 2 variables tipo double (marca es double)
+		//que vamos a comapara, no haría falta
+		double marcac1 = c1.getMarca();
+		double marcac2 = c2.getMarca();
+		
+		//si el 1º es menor que el segundo devuekve -1,
+		//si es mayor 1 y su son iguales 0, consulta el API
+		if(marcac1 < marcac2) {
+			return -1;
+		}else {
+			if(marcac1 > marcac2) {
+				return 1;
+			}
+			return 0;
+		}
+	}
+	
+}
---
+++ Unidad5/EjemplosUD5/src/ejemploOrdenar/Principal.java
@@ -0,0 +1,25 @@
+package ejemploOrdenar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int op, pos=0;
+		
+		//Colección de corredores
+		List<Corredor> corredores = new ArrayList<Corredor>();
+		
+		corredores.add(new Corredor(4, 1.43, "Jesse Owens"));
+		corredores.add(new Corredor(8, 1.11, "Ángel Naranjo"));
+		corredores.add(new Corredor(3, 2.23, "Abel Antón"));
+		corredores.add(new Corredor(1, 1.12, "Luis Miguel López"));
+		corredores.add(new Corredor(6, 1.14, "Miguel Campos"));
+		
+		//PEDIR A BRUNO LO DEL SORT Y LO DEL SORT JUNTO A LA INTANCIA DEL NO NATURAL
+	}
+
+}
