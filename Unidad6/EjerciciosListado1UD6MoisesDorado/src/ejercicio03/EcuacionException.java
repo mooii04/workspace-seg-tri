1b77da05d69620380ec0723304aa875b71c4cb59
MoisesDorado <dorado.gumoi24@triana.salesianos.edu>
2024-03-18 12:28:38 +0100

    asdasd

---
+++ Unidad6/EjerciciosListado1UD6MoisesDorado/src/ejercicio03/EcuacionException.java
@@ -0,0 +1,13 @@
+package ejercicio03;
+
+public class EcuacionException extends Exception{
+
+	public EcuacionException () {
+		super ();
+	}
+	
+	public EcuacionException (String m) {
+		super (m);
+	}
+	
+}
--- Unidad6/EjerciciosListado1UD6MoisesDorado/src/ejercicio03/Principal.java
+++ Unidad6/EjerciciosListado1UD6MoisesDorado/src/ejercicio03/Principal.java
@@ -1,10 +1,23 @@
 package ejercicio03;
 
+import utilidades.Leer;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		double a, b, c;
+		
+		System.out.println("Diga la a de la ecuación");
+		a = Leer.datoDouble();
+		
+		System.out.println("Diga la b de la ecuación");
+		b = Leer.datoDouble();
+		
+		System.out.println("Diga la c de la ecuación");
+		c = Leer.datoDouble();
+		
 		
 		
 	}
