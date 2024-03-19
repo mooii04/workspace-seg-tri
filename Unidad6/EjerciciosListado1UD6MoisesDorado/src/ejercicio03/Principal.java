23e3eb5acdf8746d6269893e457f82dbd2ce107a
MoisesDorado <dorado.gumoi24@triana.salesianos.edu>
2024-03-19 09:28:23 +0100

    asasd

--- Unidad6/EjerciciosListado1UD6MoisesDorado/src/ejercicio03/Principal.java
+++ Unidad6/EjerciciosListado1UD6MoisesDorado/src/ejercicio03/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		double a, b, c;
+		double a, b, c, x;
 		
 		System.out.println("Diga la a de la ecuación");
 		a = Leer.datoDouble();
@@ -18,7 +18,22 @@ public class Principal {
 		System.out.println("Diga la c de la ecuación");
 		c = Leer.datoDouble();
 		
+		System.out.println("Diga la x de la ecuación");
+		x = Leer.datoDouble();
 		
+		CEcuacion2grado ce = new CEcuacion2grado(a, b, c);
+		
+		try {
+			if(a==0&&b==0){
+				throw new EcuacionException("fsdbhyfs");
+			}
+			if((Math.sqrt(Math.pow(b, 2))-4*a*c)<0) {
+				
+			}
+			System.out.println(ce.formulaMas(x));
+		}catch(EcuacionException e) {
+			System.err.println(e.getMessage());
+		}
 		
 	}
 
